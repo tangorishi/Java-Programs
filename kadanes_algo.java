9f7bbf63a486e2a9a0dde30c6368547b72bc2084
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2023-01-24 00:07:32 +0530

    i have created the pull request kindly accept it

--- kadanes_algo.java
+++ kadanes_algo.java
@@ -1,30 +1,24 @@
 // Java program to print largest contiguous array sum
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
-class Kadane {
-	// Driver Code
-	public static void main(String[] args)
-	{
-		int[] a = { -2, -3, 4, -1, -2, 1, 5, -3 };
-		System.out.println("Maximum contiguous sum is "
-						+ maxSubArraySum(a));
-	}
-
-	// Function Call
-	static int maxSubArraySum(int a[])
-	{
-		int size = a.length;
-		int max_so_far = Integer.MIN_VALUE, max_ending_here
-											= 0;
-
-		for (int i = 0; i < size; i++) {
-			max_ending_here = max_ending_here + a[i];
-			if (max_so_far < max_ending_here)
-				max_so_far = max_ending_here;
-			if (max_ending_here < 0)
-				max_ending_here = 0;
-		}
-		return max_so_far;
-	}
+class tangorishi
+{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the number of elements in the array: ");
+    int n = sc.nextInt();
+    int[] a = new int[n];
+    System.out.println("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+      a[i] = sc.nextInt();
+    }
+    int max_so_far = a[0];
+    int curr_max = a[0];
+    for (int i = 1; i < n; i++) {
+      curr_max = Math.max(a[i], curr_max + a[i]);
+      max_so_far = Math.max(max_so_far, curr_max);
+    }
+    System.out.println("The largest contiguous sum is " + max_so_far);
+  }
 }
+
